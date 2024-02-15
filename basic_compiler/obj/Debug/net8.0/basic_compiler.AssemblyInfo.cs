da8562583776b983819e11a7cdb9bfb40c7fd40d
Anni HUANG <1197749338@qq.com>
2024-02-15 17:01:03 +0800

    Add solution to root

--- basic_compiler/obj/Debug/net8.0/basic_compiler.AssemblyInfo.cs
+++ basic_compiler/obj/Debug/net8.0/basic_compiler.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("basic_compiler")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f18f95985cbb6f44934c9de1ac6f51250fc8348a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7c816454cefc484a5b931bd014f597b17fa0a08")]
 [assembly: System.Reflection.AssemblyProductAttribute("basic_compiler")]
 [assembly: System.Reflection.AssemblyTitleAttribute("basic_compiler")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
